e00e5eeb01360d56702550223bb74c758b8581ba
shellywang <147886742@qq.com>
2018-01-19 15:19:35 +0800

    创建spring-mvc

---
+++ dev_framework/src/main/java/com/shelly/spring/mvc/login/LoginController.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.shelly.spring.mvc.login;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * @author zlwang
+ *
+ */
+@Controller
+public class LoginController {
+
+	@RequestMapping("/login")
+	public String login() {
+		return null;
+	}
+
+}
